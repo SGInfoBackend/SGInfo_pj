538dcabee170541bc5a27c870e134c146455b1d8
lwinITH <lwinmoe260lm@gmail.com>
2022-07-20 09:07:50 +0630

    hide modal and toastar notification

--- app/Http/Livewire/RoomComponent.php
+++ app/Http/Livewire/RoomComponent.php
@@ -3,13 +3,14 @@
 namespace App\Http\Livewire;
 
 use App\Models\RentHouse;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class RoomComponent extends Component
 {
     public function render()
     {
-        $rooms = RentHouse::orderBy('Rent_House_ID', 'DESC')->paginate(5);
+        $rooms = RentHouse::where('USER_ID', Auth::user()->id)->orderBy('Rent_House_ID', 'DESC')->paginate(5);
         return view('livewire.room-component',['rooms' => $rooms])->layout('layouts.base');
     }
 }
